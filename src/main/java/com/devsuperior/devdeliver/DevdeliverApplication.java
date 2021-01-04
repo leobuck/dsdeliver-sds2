73b3afa55c3381bea0859c9fa8dd9c3b191d0f59
Leo Buck <leonardo@timais.com>
2021-01-04 19:59:53 -0300

    Project created

---
+++ src/main/java/com/devsuperior/devdeliver/DevdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdeliverApplication.class, args);
+	}
+
+}
