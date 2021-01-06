e62e13e943f3da302decc66155a4e3df00df7c9e
Leo Buck <leonardo@timais.com>
2021-01-06 13:15:48 -0300

    GET /orders

---
+++ src/main/java/com/devsuperior/devdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.devdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.devdeliver.dto.OrderDTO;
+import com.devsuperior.devdeliver.entities.Order;
+import com.devsuperior.devdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
